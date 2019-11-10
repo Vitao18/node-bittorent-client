0377cb9d8f53c0c31196dae147382a24babb282f
Vitao18 <victoronline9@gmail.com>
2019-11-10 12:41:55 -0300

    feat: get list of peers

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const bencode = require('bencode');
+const fs = require("fs");
+const bencode = require("bencode");
+const tracker = require("./tracker");
 
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.announce.toString('utf8'));
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+
+tracker.getPeers(torrent, peers => {
+  console.log("list of peers: ", peers);
+});
---
+++ tracker.js
@@ -0,0 +1,33 @@
+const dgram = require("dgram");
+const Buffer = require("buffer").Buffer;
+const urlParse = require("url").parse;
+
+const udpSend = (socket, message, rawUrl, callback = () => {}) => {
+  const url = urlParse(rawUrl);
+  socket.send(message, 0, message.length, url.port, url.host, callback);
+};
+
+const respType = resp => {};
+const buildConnReq = () => {};
+const parseConnResp = () => {};
+const buildAnnounceReq = () => {};
+const parseAnnounceResp = () => {};
+
+module.exports.getPeers = (torrent, callback) => {
+  const socket = dgram.createSocket("udp4");
+  const url = torrent.announce.toString("utf8");
+
+  udpSend(socket, buildConnReq(), url);
+
+  socket.on("message", response => {
+    if (respType(response) === "connect") {
+      const connResp = parseConnResp(response);
+      const announceReq = buildAnnounceReq(connResp.connectionId);
+      udpSend(socket, announceReq, url);
+    } else if (respType(response) === "announce") {
+      const announceResp = parseAnnounceResp(response);
+
+      callback(announceResp.peers);
+    }
+  });
+};
